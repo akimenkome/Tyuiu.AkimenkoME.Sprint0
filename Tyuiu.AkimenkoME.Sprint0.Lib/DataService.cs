d783184343f7f5114bace113ae8573a920da0265
Tyuiu.AkimenkoME@outlook.com <akimenkom777@mail.ru>
2023-09-25 22:07:44 +0500

    Task2

---
+++ Tyuiu.AkimenkoME.Sprint0.Lib/DataService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.AkimenkoME.Sprint0.Lib
+{
+    public class DataService
+    {
+        public static string GetMessage(string name)
+        {
+            return $"Привет..., {name}";
+        }
+        
+    }
+}
---
+++ Tyuiu.AkimenkoME.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.AkimenkoME.Sprint0.Lib;
+
+namespace Tyuiu.AkimenkoME.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Мария";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет..., Мария", res);
+        }
+    }
+}
