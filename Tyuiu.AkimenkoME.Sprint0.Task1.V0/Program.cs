02864742ec7a25d319d5507d86ae7dae05225961
Tyuiu.AkimenkoME@outlook.com <akimenkom777@mail.ru>
2023-09-25 20:22:53 +0500

    Task1

---
+++ Tyuiu.AkimenkoME.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.AkimenkoME.Sprint0.Task1.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world");
+            string name = "Мария";
+            int age = 18;
+            Console.WriteLine("Здравствуйте, меня зовут " + name + " мне " + age);
+            Console.ReadKey();
+        }
+    }
+}
